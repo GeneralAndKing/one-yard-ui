98e5b69571bd4990693d632e0c70e5c44b78a857
EchoCow <lizhongyue248@163.com>
2019-10-31 21:40:19 +0800

    :sparkles: Add pug!

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,14 @@
-<template>
-  <v-app>
-    <v-app-bar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        text
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-app-bar>
-
-    <v-content>
-      <HelloWorld/>
-    </v-content>
-  </v-app>
+<template lang="pug">
+  v-app
+    v-app-bar(app)
+      v-toolbar-title.headline.text-uppercase
+        span Vuetify
+        span.font-weight-light MATERIAL DESIGN
+      v-spacer
+      v-btn(text, href="https://github.com/vuetifyjs/vuetify/releases/latest", target="_blank")
+        span.mr-2 Latest Release
+    v-content
+      HelloWorld
 </template>
 
 <script>
