c8ee97b98cd05d70c945c13175ef1b1d615e611b
EchoCow <lizhongyue248@163.com>
2019-11-03 23:58:57 +0800

    :lipstick: update ui.

--- src/views/pages/Login.vue
+++ src/views/pages/Login.vue
@@ -3,7 +3,7 @@
     v-card#form-card.px-6.pt-6.pb-7.px-sm-10.pt-sm-12.pb-sm-9.mx-auto(outlined)
       v-spacer
       v-card-title.justify-center.headline {{window === 0 ? "登录" : "欢迎"}}
-      #login-user.pb-2.text-center
+      #login-user.pb-2.text-center(style="height:44px")
         v-btn(v-if="window === 1", outlined, rounded, @click="previous") lizhongyue248@163.com
       v-spacer
       v-card-subtitle.text-center.subtitle-1.pt-0 {{window === 0 ? "使用您的帐号进行登录" : "要继续操作，请首先验证登录者是您本人"}}
@@ -16,8 +16,8 @@
           v-card-text.px-0
             v-text-field(label="输入您的密码", name="password", type="password", outlined :rules="passwordRules" ref="password" v-model='user.password')
       v-card-actions.px-0
-        a(@click="handleCreate") 创建帐号
-        a {{retrieveText}}
+        a(v-if="window === 0", @click="handleCreate") 创建帐号
+        a(v-else) 忘记密码？
         v-spacer
         v-btn(color="primary", @click="next") {{loginText}}
       v-footer#form-footer.grey--text @ 2019 copy right
