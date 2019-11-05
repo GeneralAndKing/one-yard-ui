532cc22c75585373aeb9ffd7d5991b3814429939
EchoCow <lizhongyue248@163.com>
2019-11-06 00:31:57 +0800

    :lipstick: Add MaterialPlanCreate UI

--- src/router/modules/admin.js
+++ src/router/modules/admin.js
@@ -48,6 +48,15 @@ export default {
         icon: 'how_to_vote'
       },
       component: () => import(/* webpackChunkName: "sys-department" */ '_v/sys-department')
+    },
+    {
+      path: 'materialPlanCreate',
+      name: 'materialPlanCreate',
+      meta: {
+        meta: '创建计划表',
+        icon: 'location_city'
+      },
+      component: () => import(/* webpackChunkName: "sys-department" */ '_v/material-plan-create')
     }
   ]
 }
