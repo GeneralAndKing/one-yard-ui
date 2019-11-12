0946f8b5a98af066453272825787d716fe45aa76
EchoCow <lizhongyue248@163.com>
2019-11-12 14:03:48 +0800

    :lipstick: Add error page.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
   v-app#inspire
-    v-fade-transition
+    v-fade-transition(hide-on-leave)
       router-view
 </template>
 
--- src/views/Error.vue
+++ src/views/Error.vue
@@ -1,3 +1,23 @@
 <template lang="pug">
-    a 谁他妈买小米
+    v-content#one-error
+      v-responsive.fill-height.mx-auto(max-width="1024px")
+        v-container.fill-height
+          v-layout(align-center, justify-center)
+            v-flex.text-center
+              h1.display-1.blue--text 哎呀，出错了！ 404
+              p.mt-2 你访问的页面不存在哦～
+              v-btn(outlined, color="primary", @click="handleBack") 点击返回
 </template>
+
+<script>
+export default {
+  name: 'error',
+  data: () => ({
+  }),
+  methods: {
+    handleBack () {
+      this.$router.push({ name: 'login' })
+    }
+  }
+}
+</script>
