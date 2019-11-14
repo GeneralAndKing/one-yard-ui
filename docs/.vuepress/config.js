3f1f8997a0680842b650c566a9d01e2c73f490f6
EchoCow <lizhongyue248@163.com>
2019-11-14 20:12:54 +0800

    :rocket: Deploy docs!

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -7,9 +7,11 @@ module.exports = {
   },
   base: '/one-yard-ui/',
   head: [
-    ['link', { rel: 'icon', href: '/favicon.ico' }],
+    ['link', { rel: 'apple-touch-icon', href: '/apple-touch-icon.png' }],
+    ['link', { rel: 'icon', href: '/favicon-32x32.png' }],
     ['link', { rel: 'manifest', href: '/manifest.json' }],
-  ],
+    ['meta', { name: 'theme-color', content: '#ffffff' }]
+],
   plugins: {
     '@vuepress/pwa': {
       serviceWorker: true,
@@ -17,19 +19,19 @@ module.exports = {
         message: '内容已经更新啦～',
         buttonText: '跟我走！'
       }
-    }
+    },
+    '@vuepress/back-to-top': true
   },
   themeConfig: {
-    editLink: true,
     repo: 'GeneralAndKing/one-yard-ui',
     editLinkText: '帮助我们改进此文档！',
+    editLinks: true,
     docsDir: 'docs',
     nav: [
       { text: '主页', link: '/' },
       { text: '需求分析', link: '/demand/' },
       { text: '技术架构', link: '/architecture/' },
-      { text: '关于', link: '/about/' },
-      { text: 'Github', link: 'https://github.com/GeneralAndKing' }
+      { text: '关于', link: '/about/' }
     ],
     sidebar: {
       '/demand/': [
