aeef94c742be6ebc1b905b4720fbc9b641022a78
RuiFG <905153643@qq.com>
2019-11-29 20:56:29 +0800

    全局进度条

--- src/main.js
+++ src/main.js
@@ -4,12 +4,11 @@ import router from '_router'
 import store from '_store'
 import vuetify from '_plugins/vuetify'
 import Message from '_plugins/global-message'
-import Print from '_plugins/print'
 import GlobalLoading from '_plugins/global-loading'
 import '_plugins/permissions'
+import '_plugins/print'
 import _ from 'lodash'
 GlobalLoading.config()
-Vue.use(Print) // 注册
 Vue.prototype.$loading = GlobalLoading.install
 Vue.prototype.$message = Message.install
 Vue.config.productionTip = false
